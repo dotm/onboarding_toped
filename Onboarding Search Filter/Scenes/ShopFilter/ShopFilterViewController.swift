13875a3cc5924a7a8ae9d3e378693f21f425fa28
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2019-04-10 14:46:03 +0700

    implement shop filter page layout

--- Onboarding Search Filter/Scenes/ShopFilter/ShopFilterViewController.swift
+++ Onboarding Search Filter/Scenes/ShopFilter/ShopFilterViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 import RxCocoa
 import RxSwift
+import SimpleCheckbox
 
 class ShopFilterViewController: UIViewController {
+    private weak var goldMerchantCheckboxRow: UIView!
     
     private let disposeBag = DisposeBag()
     let viewModel: ShopFilterViewModel
@@ -26,8 +28,14 @@ class ShopFilterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
+        setupLayout()
         bindViewModel()
     }
+    
+    private func bindViewModel() {
+        
+    }
+    
     private func setupNavBar() {
         self.navigationItem.title = "Shop Type"
         self.navigationItem.leftBarButtonItem = closeButton
@@ -41,8 +49,61 @@ class ShopFilterViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
-    private func bindViewModel() {
+    private func setupLayout() {
+        setupGoldMerchantCheckboxRow()
+        setupOfficialStore(previousElement: goldMerchantCheckboxRow)
+    }
+    private func setupGoldMerchantCheckboxRow() {
+        let goldMerchantCheckboxRow = UITableViewCell(style: .value1, reuseIdentifier: "gold merchant row")
+        goldMerchantCheckboxRow.textLabel?.text = "Gold Merchant"
+        goldMerchantCheckboxRow.backgroundColor = .white
+        view.addSubview(goldMerchantCheckboxRow)
         
+        goldMerchantCheckboxRow.translatesAutoresizingMaskIntoConstraints = false
+        goldMerchantCheckboxRow.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        goldMerchantCheckboxRow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        goldMerchantCheckboxRow.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        goldMerchantCheckboxRow.heightAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(50)).isActive = true
+        
+        self.goldMerchantCheckboxRow = goldMerchantCheckboxRow
+        
+        let goldMerchantCheckbox = Checkbox()
+        goldMerchantCheckbox.checkedBorderColor = .black
+        goldMerchantCheckbox.uncheckedBorderColor = .tpGreen
+        goldMerchantCheckbox.checkmarkColor = .tpGreen
+        goldMerchantCheckbox.checkmarkStyle = .square
+        goldMerchantCheckboxRow.addSubview(goldMerchantCheckbox)
+        
+        goldMerchantCheckbox.translatesAutoresizingMaskIntoConstraints = false
+        goldMerchantCheckbox.widthAnchor.constraint(equalToConstant: CGFloat(20)).isActive = true
+        goldMerchantCheckbox.heightAnchor.constraint(equalToConstant: CGFloat(20)).isActive = true
+        goldMerchantCheckbox.centerYAnchor.constraint(equalTo: goldMerchantCheckboxRow.centerYAnchor).isActive = true
+        goldMerchantCheckbox.trailingAnchor.constraint(equalTo: goldMerchantCheckboxRow.trailingAnchor, constant: -CGFloat(20)).isActive = true
+    }
+    private func setupOfficialStore(previousElement: UIView) {
+        let officialStoreCheckboxRow = UITableViewCell(style: .value1, reuseIdentifier: "official store row")
+        officialStoreCheckboxRow.textLabel?.text = "Official Store"
+        officialStoreCheckboxRow.backgroundColor = .white
+        view.addSubview(officialStoreCheckboxRow)
+        
+        officialStoreCheckboxRow.translatesAutoresizingMaskIntoConstraints = false
+        officialStoreCheckboxRow.topAnchor.constraint(equalTo: previousElement.bottomAnchor).isActive = true
+        officialStoreCheckboxRow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        officialStoreCheckboxRow.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        officialStoreCheckboxRow.heightAnchor.constraint(greaterThanOrEqualToConstant: CGFloat(50)).isActive = true
+        
+        let officialStoreCheckbox = Checkbox()
+        officialStoreCheckbox.checkedBorderColor = .black
+        officialStoreCheckbox.uncheckedBorderColor = .tpGreen
+        officialStoreCheckbox.checkmarkColor = .tpGreen
+        officialStoreCheckbox.checkmarkStyle = .square
+        officialStoreCheckboxRow.addSubview(officialStoreCheckbox)
+        
+        officialStoreCheckbox.translatesAutoresizingMaskIntoConstraints = false
+        officialStoreCheckbox.widthAnchor.constraint(equalToConstant: CGFloat(20)).isActive = true
+        officialStoreCheckbox.heightAnchor.constraint(equalToConstant: CGFloat(20)).isActive = true
+        officialStoreCheckbox.centerYAnchor.constraint(equalTo: officialStoreCheckboxRow.centerYAnchor).isActive = true
+        officialStoreCheckbox.trailingAnchor.constraint(equalTo: officialStoreCheckboxRow.trailingAnchor, constant: -CGFloat(20)).isActive = true
     }
 
 }
